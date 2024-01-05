c226644e3d527a9f9729a504a31748e52b1e19ca
dabernardes <132487346+dabernardes@users.noreply.github.com>
2024-01-05 08:17:38 -0300

    atualizacao
    correcao do time do personagem ash em todas as geracoes

--- js/script1.js
+++ js/script1.js
@@ -222,25 +222,58 @@ function updateDiv(pokemon, divId) {
 function handleClick() {
     const divs = ['poke1', 'poke2', 'poke3', 'poke4', 'poke5', 'poke6'];
     const escolha = document.getElementById(ELEMENT_IDS.escolha).value;
-    const selecao = document.getElementById(ELEMENT_IDS.selecao).value;
+    let radios = document.getElementsByName("a");
+    let selecao;
+    for(let i = 0; i < radios.length; i++) {
+        if(radios[i].checked) {
+            selecao = radios[i].value;
+            break;
+        }
+    }
     let maxId;
     let ids;
     
     switch (escolha) {
         case '1º GERAÇÃO':
             ids = Array.from({length: 151}, (_, i) => i + 1);
+            if( selecao === 'person'){
+                const lperson = document.getElementById(ELEMENT_IDS.lperson).value;
+                if(lperson === 'Ash'){
+                    ids = ids.filter(id => [1, 4, 5, 6, 7, 10, 11, 12, 20, 25, 57, 89, 93, 98, 99, 122, 128, 131, 143].includes(id));
+             }}
             break;
         case '2º GERAÇÃO':
             ids = Array.from({length: 100}, (_, i) => i + 152);
+            if( selecao === 'person'){
+                const lperson = document.getElementById(ELEMENT_IDS.lperson).value;
+                if(lperson === 'Ash'){
+                    ids = ids.filter(id => [214, 152, 153, 155, 156, 158, 164, 231, 232, 190, 207].includes(id));
+             }} 
             break;
+
         case '3º GERAÇÃO':
             ids = Array.from({length: 135}, (_, i) => i + 252);
+            if( selecao === 'person'){
+                const lperson = document.getElementById(ELEMENT_IDS.lperson).value;
+                if(lperson === 'Ash'){
+                    ids = ids.filter(id => [276, 277, 252, 253, 254, 341, 324, 361, 362].includes(id));
+             }}
             break;
         case '4º GERAÇÃO':
             ids = Array.from({length: 107}, (_, i) => i + 387);
+            if( selecao === 'person'){
+                const lperson = document.getElementById(ELEMENT_IDS.lperson).value;
+                if(lperson === 'Ash'){
+                    ids = ids.filter(id => [396, 397, 398, 387, 388, 389, 390, 391, 392, 418, 472, 443 ].includes(id));
+             }}
             break;
         case '5º GERAÇÃO':
             ids = Array.from({length: 156}, (_, i) => i + 494);
+            if( selecao === 'person'){
+                const lperson = document.getElementById(ELEMENT_IDS.lperson).value;
+                if(lperson === 'Ash'){
+                    ids = ids.filter(id => [519, 520, 521, 501, 498, 499, 495, 559, 540, 541, 542, 536, 524, 525, 552, 553 ].includes(id));
+             }}
             break;
         case '6º GERAÇÃO':
             ids = Array.from({length: 72}, (_, i) => i + 650);
@@ -255,11 +288,7 @@ function handleClick() {
             ids = Array.from({length: 905}, (_, i) => i + 1);
     }
 
-     if( selecao === 'Person'){
-        const lperson = document.getElementById(ELEMENT_IDS.lperson).value;
-        if(lperson === 'Ash'){
-            ids = ids.filter(id => [1, 4, 5, 6, 7, 10, 27, 36, 78, 125, 150].includes(id));
-     }}
+    
 
     divs.forEach(div => {
         const randomId = ids[Math.floor(Math.random() * ids.length)]; 
